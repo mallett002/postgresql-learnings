7a9a7b12fe20dd3bd8d1fa6152b4d6e5c03f52b3
Will Mallett <mallett002@gmail.com>
2020-07-20 07:26:31 -0500

    adding aliases concat and challenges

--- movie_data_db/select_queries.sql
+++ movie_data_db/select_queries.sql
@@ -134,3 +134,21 @@ SELECT first_name, last_name, date_of_birth FROM actors
 WHERE gender = 'F'
 ORDER BY date_of_birth DESC
 FETCH FIRST 10 ROW ONLY;
+
+/*====================================================================
+   NULL VALUES, ALIASES AND CONCAT CHALLENGES
+====================================================================*/
+--1: Return the top 3 movies with the highest international takings.
+SELECT * FROM movie_revenues
+WHERE international_takings IS NOT NULL
+ORDER BY international_takings DESC
+LIMIT 3;
+
+--2: Concatenate the first and last names of the directors,...
+-- ...separated by a space, and call this new column "full_name".
+SELECT CONCAT_WS(' ', first_name, last_name) AS full_name FROM directors;
+
+--3: Return the actors with missing first_names or missing date_of_births.
+SELECT * FROM actors
+WHERE first_name IS NULL
+OR date_of_birth IS NULL; 
