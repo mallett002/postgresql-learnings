f2eb9ae1f90c991152a2092aa64e82a6d4e16000
Will Mallett <mallett002@gmail.com>
2020-09-07 06:34:59 -0500

    updating challenges

--- movie_data_db/string-functions/challenges.sql
+++ movie_data_db/string-functions/challenges.sql
@@ -1,20 +1,24 @@
 -- Challenges
 --1. Select the directors first and last names and movie names in upper case.
-select upper(first_name) as first_name, upper(last_name) as last_name from directors;
-
+select
+upper(d.first_name) as first_name,
+upper(d.last_name) as last_name,
+upper(m.movie_name) as movie_name
+from directors d
+join movies m on d.director_id = m.director_id;
 
 
 --2. Select the first and last names, in initial capitalization format,
 --	 ...of all the actors who have starred in a Chinese or Korean movie.
-select
+select distinct
 	initcap(a.first_name) as first_name,
 	initcap(a.last_name) last_name,
 	mo.movie_lang
 from actors a
 join movies_actors ma on ma.actor_id = a.actor_id
 join movies mo on mo.movie_id = ma.movie_id
-where mo.movie_lang = 'Chinese'
-or mo.movie_lang = 'Korean';
+where mo.movie_lang in ('Chinese', 'Korean');
+-- Distint to remove duplicates
 
 
 
